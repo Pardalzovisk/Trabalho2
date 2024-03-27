e5f247c48a4a1e622227e107cec25b0e6ed59961
Enrico Pace <202210716@unifoa.edu.br>
2024-03-27 19:19:10 -0300

    fix unittest

--- TesteTrabalho2/UnitTest1.cs
+++ TesteTrabalho2/UnitTest1.cs
@@ -2,32 +2,27 @@ using Trabalho2;
 namespace TestTrabalho2;
 
 [TestClass]
-public class UnitTest1
+public class ExerciciosTests
 {
-    [TestClass]
-    public class ExerciciosTests
-    {
-        [TestMethod]
-        public void TestSomar()
-        {
-            Exercicios exercicios = new Exercicios();
-            Assert.AreEqual(5, exercicios.Somar(2, 3));
-        }
+     [TestMethod]
+     public void TestSomar()
+     {
+         Exercicios exercicios = new Exercicios();
+         Assert.AreEqual(5, exercicios.Somar(2, 3));
+     }
 
-        [TestMethod]
-        public void TestConverterMetrosParaMilimetros()
-        {
-            MetroMM metro = new MetroMM();
-            Assert.AreEqual(5000, metro.ConverterMetrosParaMilimetros(5));
-        }
+     [TestMethod]
+     public void TestConverterMetrosParaMilimetros()
+     {
+        MetroMM metro = new MetroMM();
+        Assert.AreEqual(5000, metro.ConverterMetrosParaMilimetros(5));
+     }
 
-        [TestMethod]
-        public void TestPodeSeAposentar()
-        {
-            Aposenta aposenta = new Aposenta();
-            Assert.IsTrue(aposenta.PodeSeAposentar(65, 30));
-            Assert.IsTrue(aposenta.PodeSeAposentar(60, 25));
-            Assert.IsFalse(aposenta.PodeSeAposentar(60, 24));
-        }
-    }
+     [TestMethod]
+     public void TestPodeSeAposentar()
+     {
+        Aposenta aposenta = new Aposenta();
+        Assert.IsTrue(aposenta.PodeSeAposentar(65, 30));
+        Assert.IsFalse(aposenta.PodeSeAposentar(60, 24));
+     }
 }
